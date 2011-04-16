f34c5c9420c0d75eb06a2c216da8e8feb0256689
沈觅仁 <dickeny@gmail.com>
2011-04-17 01:18:24 +0800

    update

--- export.sh
+++ export.sh
@@ -1,7 +1,26 @@
 #!/bin/bash
 
-WWW="/var/www/nightreading"
+APP_DIR="/var/www/nightreading"
+SOCK_FILE=$APP_DIR+"/django.sock"
+PIDFILE=$APP_DIR+"/django.pid"
+
 
 git pull
-git archive  master | tar -C $WWW -xf -
+git archive  master | tar -C $APP_DIR -xf -
 
+if [ -f $PIDFILE ]; then
+    kill `cat -- $PIDFILE`
+    rm -f -- $PIDFILE
+    sleep 1
+fi
+
+set -x
+python manage.py \
+    runfcgi method=threaded \
+    debug=true --pythonpath=$APP_DIR \
+    pidfile=$PIDFILE \
+    host=127.0.0.1 port=8205
+    #maxspare=3 minspare=1 maxchildren=5 maxrequests=500 \
+    #socket=$SOCK_FILE \
+set +x
+ 
